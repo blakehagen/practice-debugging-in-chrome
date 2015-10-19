608db504867409f43a345e60af18c21201690b24
Blake Hagen <blakehagen@gmail.com>
2015-10-19 09:34:04 -0600

    practice

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function hello(){
+	return "hello";
+}
+
+// document.write(hello());
+
+
+function rev(arr){
+	return arr.reverse();
+}
+
+var arr = [1,2,3,4,5,6,7,8];
+
+document.write(rev(arr));
